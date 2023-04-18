d510d50f5edce9db3a72e3d835a9572efbf5b1d4
tsk <tanjilkh@gmail.com>
2023-04-18 10:58:52 +0100

    :zap: Proposta

--- routes.js
+++ routes.js
@@ -4,9 +4,5 @@ const router = express.Router();
 const PropostaController = require('./controllers/PropostaController');
 
 router.get('/propostas', PropostaController.buscarTodos);
-router.get('/proposta/:id', PropostaController.buscarUm);
-router.post('/proposta', PropostaController.inserir);
-router.put('/proposta/:id', PropostaController.alterar);
-router.delete('/proposta/:id', PropostaController.excluir);
 
 module.exports = router;
